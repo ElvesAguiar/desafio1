e4392d6b124e2e76a755876e8ff5412fbb771323
Elves Aguiar <elvesmhs@hotmail.com>
2023-05-15 14:15:35 -0300

    refactor: OrderServices shipment call

--- src/main/java/com/elves/com/elves/services/OrderService.java
+++ src/main/java/com/elves/com/elves/services/OrderService.java
@@ -7,9 +7,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderService {
 
-    @Autowired
-    public ShippingService shippingService;
-
     public double total(Order order) {
         return order.getBasic() - order.getDiscount()*order.getBasic()/100 + ShippingService.shipment(order);
     }
